81fda67c27b81551d59a1c06d8e5d39066da9470
Bruno Lima <brunolimadevelopment@gmail.com>
2023-12-28 01:54:01 -0300

    feat: project complete

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 const Contact = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Let's Keep in Touch</h1>
+            <h1 className={styles.title}>Lets Keep in Touch</h1>
             <div className={styles.content}>
                 <div className={styles.imgContainer}>
                     <Image src="/contact.png"
